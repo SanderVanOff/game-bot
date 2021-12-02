8927ac72092420ae8d707e296b7a85f8d5b0cc53
SanderVanOff <7okay@mail.ru>
2021-12-02 22:39:41 +0500

    усложненное дз

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 // "Загадывание случайного числа от 1 до 100"
 const bot = function(){
     const n = Math.round(Math.random() * 100);
+    let count = 10;
     function game(){
         let answer = +prompt('Угадай число от 1 до 100');
-        console.log(n);
-        if(answer === 0) {
+        if(count <= 1) {
+            let isRepeat = confirm("Попытки закончились, хотите сыграть еще?")
+            isRepeat ? bot() : answer = 0;
+        } else if(answer === 0) {
             alert("Игра окончена");
             return;
         } else if(answer > n) {
-            alert("Загаданное число меньше");
+            count--;
+            alert(`Загаданное число меньше, осталось попыток ${count}`);
             game();
         } else if (answer < n) {
-            alert("Загаданное число больше");
+            count--;
+            alert(`Загаданное число больше, осталось попыток ${count}`);
             game();
         } else if(isNaN(answer)) {
             alert("Введи число!");
             game();
         } else if( answer === n) {
-            alert( "Поздравляю, Вы угадали!!!");
-            return;
+            let isRepeat = confirm( "Поздравляю, Вы угадали!!!, Хотели бы сыграть еще?");
+            isRepeat ? bot() : answer = 0;
+            // return;
         }
     }
     game();
